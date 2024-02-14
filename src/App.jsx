1bc32599266aae30c5dad2f789aa0a68d6a5b5da
Hamdi Adan <hhhaji013@gmail.com>
2024-02-14 13:41:50 +0300

    uploading files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus, fugiat. Doloribus illum repudiandae obcaecati! Eligendi, suscipit doloribus neque maxime exercitationem aliquid blanditiis, facere amet quae porro iure magnam iste cupiditate.</p>
+      <button>Save</button>
+
+    </>
+  )
+}
+
+export default App
