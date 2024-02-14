9f7b03ae7fd944ad1962090de88211b6fbf63345
Hamdi Adan <hhhaji013@gmail.com>
2024-02-14 17:31:49 +0300

    Updating componenets

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import { Button,Stack } from 'react-bootstrap'
 import Container  from 'react-bootstrap/Container'
 import BudgetCard from './BudgetCard'
@@ -8,13 +7,14 @@ import BudgetCard from './BudgetCard'
 function App() {
   return <Container className='my-4'>
     <Stack direction="horizontal" gap="2" className="mb-4">
-      <h1 className='me-auto'>Budget</h1>
+      <h1 className='me-auto'>Budgets</h1>
       <Button variant='primary'>Add Budget</Button>
       <Button variant='outline-primary'>Add Expense</Button>
 
     </Stack>
     <div style={{display:"grid", gridTemplateColumns:"repeat(auto-fill, minmax(300px, 1fr))", gap:"1rem", alignItems:'flex-start'}}>
-     
+     <BudgetCard name="Entertainment" amount ={200} max={1000}></BudgetCard>
+
     </div>
   </Container>
   
--- src/components/BudgetCard.jsx
+++ src/components/BudgetCard.jsx
@@ -1,12 +1,13 @@
 import { Card, CardBody } from "react-bootstrap"
+import { currencyFormatter } from "./Utils"
 
 function BudgetCard({name}){
     return(
         <Card>
             <Card.Body>
                 <Card.Title>
-                    <div></div>
-                    <div></div>
+                    <div>{name}</div>
+                    <div>{currencyFormatter.format(amount)} / {currencyFormatter.format(max)}</div>
                 </Card.Title>
             </Card.Body>
         </Card>
