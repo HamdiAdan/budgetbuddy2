9a202a0d1f37a38c3a57aaf5255efa0de0e88cb7
Hamdi Adan <hhhaji013@gmail.com>
2024-02-15 13:22:35 +0300

    Completed the budget app

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
+import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BudgetProvider } from './contexts/BudgetContext'
+import { BudgetsProvider } from './contexts/BudgetsContext.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BudgetProvider>
+    <BudgetsProvider>
        <App />
-    </BudgetProvider>
+    </BudgetsProvider>
     
   </React.StrictMode>,
 )
