8e1e5e8f32056071427dab6898347992ebe0f525
Hamdi Adan <hhhaji013@gmail.com>
2024-02-19 10:49:36 +0300

    uploading test

---
+++ src/App.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  test('renders without crashing', () => {
+    const { getByText } = render(<App />);
+    
+    // Check if "Budgets" text is present
+    const budgetsText = getByText('Budgets');
+    expect(budgetsText).toBeInTheDocument();
+    
+    // Check if "Add Budget" button is present
+    const addButton = getByText('Add Budget');
+    expect(addButton).toBeInTheDocument();
+    
+    // Check if "Add Expense" button is present
+    const addExpenseButton = getByText('Add Expense');
+    expect(addExpenseButton).toBeInTheDocument();
+  });
+
+  test('toggles dark mode when the "Dark Mode" button is clicked', () => {
+    const { getByText, container } = render(<App />);
+    
+    
+    expect(document.body).not.toHaveClass('dark-mode');
+    
+    // Click on the "Dark Mode" button
+    const darkModeButton = getByText('Dark Mode');
+    fireEvent.click(darkModeButton);
+    
+    // After clicking, dark mode should be enabled
+    expect(document.body).toHaveClass('dark-mode');
+    
+    // Click again to disable dark mode
+    fireEvent.click(darkModeButton);
+    expect(document.body).not.toHaveClass('dark-mode');
+  });
+});
